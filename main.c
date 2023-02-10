d9b92d7913b7b8df863d46d42826fc8b65ed4756
Narayan Samal <narayancoding@gmail.com>
2023-02-10 19:12:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  class
+//
+//  Created by Narayan Samal on 10/02/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ prog1.c
@@ -0,0 +1,8 @@
+//
+//  prog1.c
+//  class
+//
+//  Created by Narayan Samal on 10/02/23.
+//
+
+#include <stdio.h>
